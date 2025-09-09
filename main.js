74e07c926aa09481fc6e0df2ac97b3d9a088089c
mariaeduardaaaaaaaa <146455966+mariaeduardaaaaaaaa@users.noreply.github.com>
2025-09-09 10:43:07 -0300

    09/09/2025

--- main.js
+++ main.js
@@ -65,6 +65,9 @@ function geraSenha() {
 }
 
 function classificaSenha() {
+
+    let entropia = tamanhoSenha*Math.log2(tamanhoSenha);
+
     forcaSenha.classList.remove('fraca', 'media', 'forte');
     if (tamanhoSenha > 11) {
         forcaSenha.classList.add('forte');
